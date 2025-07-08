1935bdd874a1cc6f69078497e1892c5691dd1b05
Téo Derboven <teoderboven@outlook.com>
2025-07-08 02:01:19 +0200

    Service: TimeUpdateService: fix bad call to cast function

--- src/service/TimeUpdateService.cpp
+++ src/service/TimeUpdateService.cpp
@@ -21,7 +21,7 @@ namespace PiAlarm::service {
         // use a time_point to specify the exact next second instant to wait until,
         // ensuring precise synchronization with the system clock,
         // rather than waiting a fixed duration which can accumulate drift.
-        auto next_tick = time_point_cast<seconds>{now} + seconds{1};
+        auto next_tick = time_point_cast<seconds>(now) + seconds{1};
 
         // if we’re already past next tick (possible with low-precision clocks), add another second
         if (next_tick <= now) {
