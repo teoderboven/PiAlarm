fd60464a7cd5cbb832f82b18fb391542761d583b
Téo Derboven <teoderboven@outlook.com>
2025-07-23 23:31:28 +0200

    Refactor: main: add run function

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,28 @@
 #include "service/TimeUpdateService.h"
 #include "service/WeatherApiService.h"
 
+namespace PiAlarm {
+    // Forward declaration of the run function
+    [[noreturn]]
+    void run(view::ViewManager& viewManager);
+}
+
 using namespace PiAlarm;
 
-[[ noreturn ]]
+/**
+ * Main function for the PiAlarm application.
+ * Initializes models, controllers, providers, views, and services.
+ * Starts the main application loop.
+ *
+ * @param argc Argument count
+ * @param argv Argument vector
+ * @return Exit code (never returns)
+ */
+[[noreturn]]
 int main(int argc, char *argv[]) {
 #ifdef DISPLAY_CONSOLE
     utils::setup_console();
 #endif
-
 
     // Models
     model::AlarmData alarm_data;
@@ -58,9 +72,29 @@ int main(int argc, char *argv[]) {
     weatherApiService.start();
 
     // Main application loop
-    while (true) {
-        viewManager.loop();
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(333)); // Approx. 30 FPS
-    }
+    run(viewManager);
 }
+
+/**
+ * @namespace PiAlarm
+ * @brief The main namespace for the PiAlarm application.
+ */
+namespace PiAlarm {
+
+    /**
+     * Runs the main application loop.
+     * This function continuously updates the view manager and sleeps for a short duration
+     * to control the frame rate.
+     *
+     * @param viewManager The view manager to update.
+     */
+    [[noreturn]]
+    void run(view::ViewManager& viewManager) {
+        while (true) {
+            viewManager.loop();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(333)); // Approx. 30 FPS
+        }
+    }
+
+} // namespace PiAlarm
